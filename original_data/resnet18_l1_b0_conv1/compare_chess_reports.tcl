4bab0744b57eb75291e95c13a4d9cb06a5661296
basti <basti.preuss95@gmail.com>
2020-03-03 11:51:38 +0100

    delete code

--- original_data/resnet18_l1_b0_conv1/compare_chess_reports.tcl
+++
@@ -1,4 +0,0 @@
-set appname benchmark_conv2d
-source /home/basti/data/hdd/test/cnn_asip/core/bin/verification_helpers.tcl
-compare_chess_reports "logs/run_native.log" "chess_reports/$appname.mem" "chess_reports/$appname.diff" ;
-exit 0
